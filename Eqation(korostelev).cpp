04292760b46ab10a59e8342fbc2f897868354f02
KLOWN228 <70750336+KLOWN228@users.noreply.github.com>
2020-09-06 15:07:33 +0300

    Add files via upload

---
+++ Eqation(korostelev).cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+
+    double a, b, c, d, x1, x2;
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите a\n";
+    cin >> a;
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите b\n";
+    cin >> b;
+
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите c\n";
+    cin >> c;
+
+    d = b * b - 4 * a * c;
+    if (d > 0)
+    {
+        x1 = ((-b) + sqrt(d)) / (2 * a);
+        x2 = ((-b) - sqrt(d)) / (2 * a);
+        cout << "x1 = " << x1 << "\n";
+        cout << "x2 = " << x2 << "\n";
+    }
+    if (d == 0)
+    {
+        x1 = -(b / (2 * a));
+        cout << "x1 = x2 = " << x1 << "\n";
+    }
+    if (d < 0)
+        setlocale(LC_ALL, "Rus");
+        cout << "D < 0, Действительных корней уравнения не существует";
+}
